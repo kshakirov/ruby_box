259e94fb91413cb8e978b86da675e169fcdfe2a5
Kyrylo Shakirov <kyrylo.shakirov@zorallabs.com>
2019-04-18 14:33:58 +0300

    # end point fixed

--- app.rb
+++ app.rb
@@ -10,12 +10,12 @@ class ConverterService < Sinatra::Base
   get '/' do
     'Testing Jruby Box!'
   end
-  post '/lambda/ruby/converter/' do
+  post '/lambda/ruby/converters' do
     body = JSON.parse request.body.read, symbolize_names: true
     settings.engine.run(body).to_json
   end
 
-  post '/lambda/ruby/script/' do
+  post '/lambda/ruby/script' do
     body = JSON.parse request.body.read, symbolize_names: true
     settings.engine.test(body).to_json
   end
